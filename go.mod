ce28899697a2d069bca9ae28bd960f0b18ed2dd9
nate smith <vilmibm@github.com>
2022-01-27 17:40:16 -0600

    add a REPL

--- go.mod
+++ go.mod
@@ -2,12 +2,22 @@ module github.com/vilmibm/gh-dungeon
 
 go 1.17
 
-require github.com/cli/go-gh v0.0.3
+require (
+	github.com/AlecAivazis/survey/v2 v2.3.2
+	github.com/cli/go-gh v0.0.3
+)
 
 require (
 	github.com/cli/safeexec v1.0.0 // indirect
 	github.com/cli/shurcooL-graphql v0.0.1 // indirect
 	github.com/henvic/httpretty v0.0.6 // indirect
+	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
+	github.com/mattn/go-colorable v0.1.2 // indirect
+	github.com/mattn/go-isatty v0.0.8 // indirect
+	github.com/mgutz/ansi v0.0.0-20170206155736-9520e82c474b // indirect
 	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
+	golang.org/x/sys v0.0.0-20210423082822-04245dca01da // indirect
+	golang.org/x/term v0.0.0-20210503060354-a79de5458b56 // indirect
+	golang.org/x/text v0.3.6 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
 	"io"
 	"os"
+	"strings"
+	"text/template"
 
+	"github.com/AlecAivazis/survey/v2"
 	"github.com/cli/go-gh"
 	"github.com/cli/go-gh/pkg/api"
 )
@@ -16,8 +21,10 @@ type IOStreams struct {
 
 type Opts struct {
 	Args   []string
+	Repo   string
 	IO     *IOStreams
-	Client api.RESTClient
+	Getter ContentGetter
+	REPL   REPL
 }
 
 type Contents struct {
@@ -46,31 +53,171 @@ func (g *RESTContentGetter) GetPathContents(path string) (Contents, error) {
 }
 
 func _main(opts *Opts) error {
-	c := opts.Client
+	g := opts.Getter
+	repl := opts.REPL
 
-	// TODO detect or take argument
-	repo := "cli/cli"
+	state := GameState{}
 
-	getter := NewRESTContentGetter(c, repo)
-
-	contents, err := getter.GetPathContents("")
+	contents, err := g.GetPathContents("")
 	if err != nil {
 		return err
 	}
 	fmt.Printf("DBG %#v\n", contents)
 
-	// TODO write REPL
+	var cmd Command
+
+	for {
+		fmt.Fprintln(opts.IO.Out, state.RenderRoom())
+		cmd, err = repl.NextCommand()
+		if err != nil && errors.Is(err, UnknownCommandError{}) {
+			fmt.Fprintln(opts.IO.Out, err.Error())
+			err = nil
+			continue
+		} else if err != nil {
+			break
+		}
+
+		if cmd.Kind == QuitCommand {
+			fmt.Fprintln(opts.IO.Out, "see you again~")
+			break
+		}
+
+		if cmd.Kind == HelpCommand {
+			fmt.Fprintln(opts.IO.Out, "supported verbs: look, go, examine, quit")
+		}
+	}
+
 	// TODO fill in Contents struct as needed
 	// TODO hash functions for repo names and file names
 
-	return nil
+	return err
+}
+
+type CommandKind string
+
+type Command struct {
+	Kind CommandKind
+	Args []string
+	Raw  string
+}
+
+type REPL interface {
+	NextCommand() (Command, error)
+}
+
+type IOREPL struct {
+	io      *IOStreams
+	history []Command
+}
+
+func NewIOREPL(io *IOStreams) *IOREPL {
+	return &IOREPL{
+		io:      io,
+		history: []Command{},
+	}
+}
+
+const (
+	LookCommand CommandKind = "look"
+	GoCommand   CommandKind = "go"
+	QuitCommand CommandKind = "quit"
+	HelpCommand CommandKind = "help"
+)
+
+func (r *IOREPL) NextCommand() (cmd Command, err error) {
+	raw := ""
+	if err = survey.AskOne(&survey.Input{
+		Message: "for help >",
+	}, &raw); err != nil {
+		return
+	}
+
+	return parseCommand(raw)
+}
+
+type UnknownCommandError struct {
+	error
+	Raw string
+}
+
+func parseCommand(raw string) (cmd Command, err error) {
+	// TODO further do parsing and produce Args
+	if strings.HasPrefix(raw, "look") {
+		cmd = Command{
+			Raw:  raw,
+			Kind: LookCommand,
+		}
+	} else if strings.HasPrefix(raw, "quit") {
+		cmd = Command{
+			Raw:  raw,
+			Kind: QuitCommand,
+		}
+	} else if strings.HasPrefix(raw, "go") {
+		cmd = Command{
+			Raw:  raw,
+			Kind: GoCommand,
+		}
+	} else if strings.HasPrefix(raw, "?") {
+		cmd = Command{
+			Raw:  raw,
+			Kind: HelpCommand,
+		}
+
+	} else {
+		err = UnknownCommandError{
+			Raw: raw,
+		}
+	}
+
+	return
+}
+
+type GameState struct {
+	Path string
+}
+
+const roomTmpl string = `
+You are standing in a room. {{ .RoomDescription }}
+
+{{ .RoomFlavor }}
+`
+
+// TODO hallway rendering
+
+func (s *GameState) RenderRoom() string {
+	// TODO pay attention to Path
+	// TODO render door
+	// TODO special handling for root room?
+	// TODO objects on ground rendering
+	tmpl, err := template.New("room").Parse(roomTmpl)
+	if err != nil {
+		panic(err.Error())
+	}
+	out := &bytes.Buffer{}
+	err = tmpl.Execute(out, struct {
+		RoomDescription string
+		RoomFlavor      string
+	}{
+		RoomDescription: "TODO ROOM DESC",
+		RoomFlavor:      "TODO ROOM FLAVOR",
+	})
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return out.String()
 }
 
 func main() {
+	// TODO detect or take argument
+	repo := "cli/cli"
+
 	io := &IOStreams{
 		In:  os.Stdin,
 		Out: os.Stdout,
 	}
+	repl := NewIOREPL(io)
 	client, err := gh.RESTClient(nil)
 	if err != nil {
 		fmt.Printf("could not create client: %s\n", err.Error())
@@ -78,8 +225,10 @@ func main() {
 	}
 	err = _main(&Opts{
 		IO:     io,
-		Client: client,
+		Repo:   repo,
+		Getter: NewRESTContentGetter(client, repo),
 		Args:   os.Args,
+		REPL:   repl,
 	})
 	if err != nil {
 		fmt.Println(err.Error())
